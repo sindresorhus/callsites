d289c109d72a9971f84976bb2d14ceb9035bdc7a
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-17 20:48:50 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 var assert = require('assert');
 var path = require('path');
-var callsites = require('./index');
+var callsites = require('./');
 
 it('should get the callsites', function () {
 	assert.strictEqual(path.basename(callsites()[0].getFileName()), 'test.js');
