a3f29c550bec85a058249e40107aeb66d5ceda3e
Sam Verschueren <sam.verschueren@gmail.com>
2018-11-17 20:10:18 +0100

    Add TypeScript definitions

---
+++ index.test-d.ts
@@ -0,0 +1,20 @@
+import {expectType} from 'tsd-check';
+import {default as callsites, CallSite} from '.';
+
+const callsite = callsites()[0];
+
+expectType<CallSite[]>(callsites());
+
+expectType<unknown | undefined>(callsite.getThis());
+expectType<string | null>(callsite.getTypeName());
+expectType<Function | null>(callsite.getFunction());
+expectType<string | null>(callsite.getFunctionName());
+expectType<string | undefined>(callsite.getMethodName());
+expectType<string | null>(callsite.getFileName());
+expectType<number | null>(callsite.getLineNumber());
+expectType<number | null>(callsite.getColumnNumber());
+expectType<string | undefined>(callsite.getEvalOrigin());
+expectType<boolean>(callsite.isToplevel());
+expectType<boolean>(callsite.isEval());
+expectType<boolean>(callsite.isNative());
+expectType<boolean>(callsite.isConstructor());
