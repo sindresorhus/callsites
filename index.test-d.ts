e40eb101b97056ddac5c599de18a35c690f3ee5e
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-20 00:31:59 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,17 +4,5 @@ import callsites, {CallSite} from '.';
 const callsite = callsites()[0];
 
 expectType<CallSite[]>(callsites());
-
 expectType<unknown | undefined>(callsite.getThis());
-expectType<string | null>(callsite.getTypeName());
-expectType<Function | null>(callsite.getFunction());
-expectType<string | null>(callsite.getFunctionName());
-expectType<string | undefined>(callsite.getMethodName());
-expectType<string | null>(callsite.getFileName());
-expectType<number | null>(callsite.getLineNumber());
-expectType<number | null>(callsite.getColumnNumber());
-expectType<string | undefined>(callsite.getEvalOrigin());
-expectType<boolean>(callsite.isToplevel());
-expectType<boolean>(callsite.isEval());
-expectType<boolean>(callsite.isNative());
 expectType<boolean>(callsite.isConstructor());
