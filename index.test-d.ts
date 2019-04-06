2f0a19668dcc455f01cffedc0b24f550c05c6914
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 13:37:47 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,6 @@
-import {expectType} from 'tsd-check';
-import callsites, {CallSite} from '.';
+import {expectType} from 'tsd';
+import callsites = require('.');
+import {CallSite} from '.';
 
 const callsite = callsites()[0];
 
