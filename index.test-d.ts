eb73a4796ed6774df3715ed6d82d1b97f963aabc
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-07 09:53:43 +0000

    Add more tests for the TypeScript definition (#12)

--- index.test-d.ts
+++ index.test-d.ts
@@ -6,4 +6,15 @@ const callsite = callsites()[0];
 
 expectType<CallSite[]>(callsites());
 expectType<unknown | undefined>(callsite.getThis());
+expectType<string | null>(callsite.getTypeName());
+expectType<Function | undefined>(callsite.getFunction());
+expectType<string | null>(callsite.getFunctionName());
+expectType<string | undefined>(callsite.getMethodName());
+expectType<string | null>(callsite.getFileName());
+expectType<number | null>(callsite.getLineNumber());
+expectType<number | null>(callsite.getColumnNumber());
+expectType<string | undefined>(callsite.getEvalOrigin());
+expectType<boolean>(callsite.isToplevel());
+expectType<boolean>(callsite.isEval());
+expectType<boolean>(callsite.isNative());
 expectType<boolean>(callsite.isConstructor());
