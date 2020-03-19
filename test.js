16b62aedf82ae4457d47ef975bfc668cf464589a
Raphael von der Grün <raphinesse@gmail.com>
2020-03-19 17:31:32 +0100

    Add nested call test (#15)

--- test.js
+++ test.js
@@ -5,3 +5,19 @@ import callsites from '.';
 test('main', t => {
 	t.is(path.basename(callsites()[0].getFileName()), 'test.js');
 });
+
+test('nested', t => {
+	const g = () => callsites();
+	const f = () => g();
+
+	const sites = f().slice(0, 3).map(site => ({
+		fileName: site.getFileName(),
+		functionName: site.getFunctionName()
+	}));
+
+	t.deepEqual(sites, [
+		{fileName: __filename, functionName: 'g'},
+		{fileName: __filename, functionName: 'f'},
+		{fileName: __filename, functionName: null}
+	]);
+});
