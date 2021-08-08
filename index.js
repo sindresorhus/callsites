41ea990709725e56aa6abc3b373e3d750b920fe2
Jimmy Wärting <jimmy@warting.se>
2021-08-08 23:31:02 +0200

    ignore error

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const callsites = () => {
 	const _prepareStackTrace = Error.prepareStackTrace;
 	Error.prepareStackTrace = (_, stack) => stack;
-	const stack = new Error('_').stack.slice(1);
+	// eslint-disable-next-line unicorn/error-message
+	const stack = new Error().stack.slice(1);
 	Error.prepareStackTrace = _prepareStackTrace;
 	return stack;
 };
