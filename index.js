67d5da25d02b5b6e9ff2adbdba0d7d633a0e71c7
Dragonizedpizza <dragon@dragonizedpizza.com>
2022-09-22 18:13:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const _prepareStackTrace = Error.prepareStackTrace;
+
 export default function callsites() {
-	const _prepareStackTrace = Error.prepareStackTrace;
 	Error.prepareStackTrace = (_, stack) => stack;
 	const stack = new Error().stack.slice(1); // eslint-disable-line unicorn/error-message
 	Error.prepareStackTrace = _prepareStackTrace;
